fccb8e9bd7197c0932d53814bcce3fe92702ed80
Ram24102002 <ramachandran24102002@gmail.com>
2024-04-16 22:15:08 +0530

    fixed footer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var input=document.getElementById("input");
 var pass=document.getElementById("password");
 var sub=document.getElementById("sub-btn");
-
+var eye=document.getElementById("fa-eye-slash")
 
 var email="User";
 var password="password";
+
 
 
 // password verification
@@ -31,5 +32,22 @@ function keypress(event)
     if(event.keyCode===13)
     {
         passwordVerify();
+    }
+}
+
+
+
+// password eye
+
+function eyeClick()
+{
+    // alert("entering")
+    if(pass.type=="password")
+    {
+        pass.type= "text";
+    }
+    else
+    {
+        pass.type="password";
     }
 }
